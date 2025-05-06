92f64002047f313ed7aaaebbe88d6cb820ec1301
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2025-05-06 15:40:22 +0530

    Initial Commit

---
+++ PhoodApp/PhoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoodAppApp.swift
+//  PhoodApp
+//
+//  Created by Rahul Ravi Prakash on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PhoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
